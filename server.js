bdfe60f209d13b04c0b96824eb33d61e7b9c685a
shreyansh <shrey@fpslounge.com>
2025-01-22 08:22:05 +0530

    join rooms feature added

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// server.js
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
 
 const app = express()
-const server = http.createServer(app) // Create an HTTP server
+const server = http.createServer(app)
 const score = []
 const io = new Server(server, {
   cors: {
@@ -16,20 +15,17 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id)
 
-  // Listen for a "message" event from the client
-  socket.on("message", (msg) => {
+  socket.on("add_player", (msg) => {
     console.log("Message received:", msg)
-    io.emit("message", msg) // Broadcast the message to all connected clients
+    io.emit("add_player", msg)
   })
   socket.on("hit", (msg) => {
     console.log("hit achieved by:", msg)
-    // calculateScore(msg)
-    io.emit("hit", msg) // Broadcast the message to all connected clients
+    io.emit("hit", msg)
   })
   socket.on("signal", (data) => {
     io.to(data.to).emit("signal", { from: socket.id, signal: data.signal })
   })
-  // Handle client disconnections
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id)
   })
